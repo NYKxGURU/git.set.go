8239f73aee5cf534419f17e1d1371c5ff8132e6f
NYKxGURU <102412806+NYKxGURU@users.noreply.github.com>
2022-03-26 17:45:24 +0530

    Update fix_me.c
    idk

--- fix_me.c
+++ fix_me.c
@@ -3,17 +3,23 @@
 
 int main() {    
 
-    int number1, number2, sum, multi;
+    int number1, number2, sum, sub, multi, div;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    sum = number1 + number2;      
+    sum = number1 + number2;    
+    
+     // calculating sub
+    sub = number1 - number2; 
 
     // Multiply number 1 and number 2
-    multi = number1 * number1
+    multi = number1 * number2;
+        
+     // Divide number 1 and number 2
+    div = number1 / number2;   
     
-    printf("%d + %d = %d", number1, number2, sum);
+    printf("%d + %d = %d", number1, number2, sum, sub, multi, div);
     return 0;
 }
